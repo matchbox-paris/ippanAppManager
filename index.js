bdc75156ecadc59c4103ca91a8227c7236f82398
grid23 <hello@grid23.net>
2017-10-05 11:04:10 +0200

    expose IniParser class to parse config files ; env variables looked in env.js

--- index.js
+++ index.js
@@ -2,5 +2,6 @@
 
 module.exports = {
     App: require("./lib/App.js").App
+  , IniParser: require("./lib/IniParser").IniParser
   , WebpackBundler: require("./lib/Webpack3Bundler").Webpack3Bundler
 }
